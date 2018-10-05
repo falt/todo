1827cbaeb8e9179cd3d1a15eb32c7a5d905163a8
falt <falt.andreas@gmail.com>
2018-10-05 18:04:47 +0200

    first

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyD1tJfCq5aohg7na9DEFe3EO0qGS9WZH8M",
+  authDomain: "todo-16a01.firebaseapp.com",
+  databaseURL: "https://todo-16a01.firebaseio.com",
+  projectId: "todo-16a01",
+  storageBucket: "todo-16a01.appspot.com",
+  messagingSenderId: "337201896646"
+};
